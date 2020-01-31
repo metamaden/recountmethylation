8434e0cddcd8d6205b7d66d7470e0c70c4e05fb4
Sean <maden@ohsu.edu>
2020-01-30 16:04:16 -0800

    revises onload.R

--- R/onload.R
+++ R/onload.R
@@ -1,23 +1,21 @@
 .onAttach = function(libname, pkgname) {
   version = packageDescription(pkgname, fields = "Version")
-  
-  msg = paste0("***
-               ", pkgname, " version ", version, "
-               #-----------
-               # DISCLAIMER
-               #-----------
-               Recount Methylation contains data from GEO, a live public database where 
-               alterations (e.g. updates, revisions, additions, and removals) to online 
-               records can cause discrepancies with stored data over time. We advise 
-               users to be vigilant in cross-checking their findings with latest available 
-               records and study data.
-               
-               #---------
-               # CITATION
-               #---------
-               If you use this package in published research, please cite the accompanying manuscript.
-               ***
-               ")	
-  
+  msg = paste0("",
+"#---------------------------------------------#
+* ", pkgname, " version ", version, 
+"* DISCLAIMER
+Recount Methylation contains data from GEO 
+(https://www.ncbi.nlm.nih.gov/geo/), a live 
+public database where alterations to online 
+records can cause discrepancies with stored 
+data over time. We make no garauntees to the 
+accuracy of data in Recount Methylation and 
+advise users cross-check their findings with 
+latest available records,
+
+* CITATION
+If you use this package in published research, 
+please cite the accompanying manuscript.
+#---------------------------------------------#")	
   packageStartupMessage(msg)
 }
