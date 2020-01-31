02bafaadae070cc1a2fe9d3079a722e2c1523e16
Sean <maden@ohsu.edu>
2020-01-30 20:36:01 -0800

    Update onload.R

--- R/onload.R
+++ R/onload.R
@@ -5,13 +5,13 @@
 \n* ", pkgname, " version ", version,
 "\n* DISCLAIMER
 Recount Methylation contains data from GEO 
-(https://www.ncbi.nlm.nih.gov/geo/), a live 
-public database where alterations to online 
-records can cause discrepancies with stored 
-data over time. We cannot gaurantee the 
-accuracy of data in Recount Methylation and 
-advise users cross-check their findings with 
-latest available records,
+(ncbi.nlm.nih.gov/geo/), a live public 
+database where alterations to online records 
+can cause discrepancies with stored data 
+over time. We cannot gaurantee the accuracy 
+of data in Recount Methylation and advise 
+users cross-check their findings with 
+latest available records.
 \n* CITATION
 If you use this package in published research, 
 please cite the accompanying manuscript.
