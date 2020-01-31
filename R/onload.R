6dd5ec10c6ecae2396f61a1346a3fd7ce948271a
Sean Maden <metamaden@users.noreply.github.com>
2020-01-31 11:22:48 -0800

    Update onload.R

--- R/onload.R
+++ R/onload.R
@@ -2,8 +2,9 @@
   version = packageDescription(pkgname, fields = "Version")
   msg = paste0("",
 "\n***************************************
-\n* ", pkgname, " version ", version,
-"\n* DISCLAIMER
+\n", pkgname, 
+"\n* VERSION:\n", version,
+"\n* DISCLAIMER:\n
 Recount Methylation contains data from GEO 
 (ncbi.nlm.nih.gov/geo/), a live public 
 database where alterations to online records 
@@ -12,7 +13,7 @@ over time. We cannot gaurantee the accuracy
 of data in Recount Methylation and advise 
 users cross-check their findings with 
 latest available records.
-\n* CITATION
+\n* CITATION:\n
 If you use this package in published research, 
 please cite the accompanying manuscript.
 ***************************************")	
