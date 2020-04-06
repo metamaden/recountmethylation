613d969506216f18fa3b560da0ad478962fc6f3f
Sean <maden@ohsu.edu>
2020-04-06 13:23:19 -0700

    commented tests

--- tests/test-gds_idat2rg.R
+++ tests/test-gds_idat2rg.R
@@ -2,5 +2,5 @@
 
 # Unit tests for gds_idat2rg()
 
-gsmvi <- c("GSM2465267", "GSM2814572")
-testthat::expect_that(gds_idat2rg(gsmvi), is_a("RGChannelSet"))
+#gsmvi <- c("GSM2465267", "GSM2814572")
+#testthat::expect_that(gds_idat2rg(gsmvi), is_a("RGChannelSet"))
--- tests/test-gds_idatquery.R
+++ tests/test-gds_idatquery.R
@@ -2,5 +2,5 @@
 
 # Unit tests for `gds_idatquery()`
 
-gsmvi <- c("GSM2465267", "GSM2814572")
-testthat::expect_equal(gds_idatquery(gsmvi)[["basenames"]], gsmvi)
+#gsmvi <- c("GSM2465267", "GSM2814572")
+#testthat::expect_equal(gds_idatquery(gsmvi)[["basenames"]], gsmvi)
