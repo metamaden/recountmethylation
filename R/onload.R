d32dff70d6511f1a77134f10350f5984b92a741d
Sean Maden <metamaden@users.noreply.github.com>
2020-04-07 11:37:46 -0700

    guarantee

--- R/onload.R
+++ R/onload.R
@@ -9,7 +9,7 @@ Recount Methylation contains data from GEO
 (ncbi.nlm.nih.gov/geo/), a live public 
 database where alterations to online records 
 can cause discrepancies with stored data 
-over time. We cannot gaurantee the accuracy 
+over time. We cannot guarantee the accuracy 
 of data in Recount Methylation and advise 
 users cross-check their findings with 
 latest available records.
