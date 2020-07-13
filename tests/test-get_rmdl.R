9405c9bd58e412f7d873b1df8e629395d71d9e7e
Sean <maden@ohsu.edu>
2020-07-13 16:24:13 -0700

    test, vers
    revises get_rmdl test
    bumps version

--- tests/test-get_rmdl.R
+++ tests/test-get_rmdl.R
@@ -3,6 +3,6 @@ testthat::context("Database file information from get_rmdl")
 testthat::test_that("File info as character string", {
 
     fileinfo <- recountmethylation::get_rmdl(download = FALSE)
-    testthat::expect_true(is.character(fileinfo))
+    testthat::expect_that(fileinfo, testthat::is_a("character"))
     
 })
