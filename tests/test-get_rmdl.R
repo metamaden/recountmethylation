3257e77f3fdf54ad5727237083695c31999694b3
Sean Maden <metamaden@users.noreply.github.com>
2020-08-10 12:54:58 -0700

    remove broken test

--- tests/test-get_rmdl.R
+++
@@ -1,8 +0,0 @@
-testthat::context("Database file information from get_rmdl")
-
-testthat::test_that("File info as character string", {
-
-    fileinfo <- recountmethylation::get_rmdl(download = FALSE)
-    testthat::expect_that(fileinfo, testthat::is_a("character"))
-    
-})
